034033a0a91c60a7131322946a7b071885e5468b
Alan <alan@alan.com>
2026-08-20 01:18:48 +0200

    feat: add some icons and section

---
+++ src/assets/iconos.ts
@@ -0,0 +1,10 @@
+export const iconos = {
+  github: "/src/assets/github.svg",
+  linkedin: "/src/assets/linkedin.svg",
+  email: "/src/assets/email.svg",
+  whatsapp: "/src/assets/whatsapp.svg",
+  telegram: "/src/assets/telegram.svg",
+  instagram: "/src/assets/instagram.svg",
+  twitter: "/src/assets/twitter.svg",
+  discord: "/src/assets/discord.svg",
+};
